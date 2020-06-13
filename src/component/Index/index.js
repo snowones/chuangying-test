57cd0ee7c127903721b1c824e9f0c9c3da6c2364
张应祥 <37978150+snowones@users.noreply.github.com>
2020-06-13 09:31:26 +0800

    1项目初始化 2为项目配置解析markdown

---
+++ src/component/Index/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import md1 from '../../md/优惠信息.md';
+import md2 from '../../md/1.md';
+import md3 from '../../md/肯德基&麦当劳早餐费6用于合并.md';
+import './index.css';
+class Index extends Component {
+    render() {
+       
+        return (
+            <div>
+                 {/* <ReactMarkdown source={md1} /> */}
+                 <img  style={{display:'none' ,width:'100px'}} src={require('../../img/1.jpg')}/>
+                 <img  style={{display:'none'}} src={require('../../img/2.jpg')}/>
+                 <img  style={{display:'none'}} src={require('../../img/3.jpg')}/>
+                 <img  style={{display:'none'}} src={require('../../img/4.png')}/>
+                 <ReactMarkdown source={md3} />
+            </div>
+
+        );
+    }
+}
+
+export default Index;
